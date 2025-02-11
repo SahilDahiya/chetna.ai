b414282b040c3b143ecd17a8f23ae00ae30bf2d6
SahilDahiya <dahiya.sahil.89@gmail.com>
2025-02-10 18:40:22 -0800

    Enhance discussion interface: implement custom console theme and improve output formatting with rich panels

--- src/cli/commands/start_discussion.py
+++ src/cli/commands/start_discussion.py
@@ -2,10 +2,21 @@ from uuid import UUID
 
 import typer
 from rich.console import Console
+from rich.panel import Panel
+from rich.theme import Theme
 
 from src.application.modules.discussions.commands import DiscussCommand
 
-console = Console(soft_wrap=True)
+custom_theme = Theme(
+    {
+        'info': 'yellow on blue',
+        'prompt': 'green italic',
+        'thinking': 'yellow',
+        'goodbye': 'red bold',
+    }
+)
+
+console = Console(theme=custom_theme)
 
 app = typer.Typer()
 
@@ -14,11 +25,22 @@ app = typer.Typer()
 def start_discussion():
     discuss = DiscussCommand()
     discuss.create_discussion(user_id=UUID('123e4567-e89b-12d3-a456-426614174000'))
-    print(discuss.passage.text_english)
-    print('Ask me anything!')
+    console.print(
+        Panel(
+            discuss.passage.text_english,
+            title=discuss.passage.book_name,
+            subtitle=f'{discuss.passage.chapter_no}#{discuss.passage.passage_no}',
+            style='info',
+            highlight=True,
+        )
+    )
+
+    console.print(Panel('Ask me anything!', title='🤖', style='prompt'))
 
     while True:
-        query = typer.prompt('Your resposnse')
+        console.print("─" * console.width, style="dim")
+        console.print("[green]You:[/green]")
+        query = input(" > ").strip()
 
         if query.lower() == 'exit':
             console.print('[bold red]Goodbye![/bold red]')
@@ -26,4 +48,4 @@ def start_discussion():
 
         with console.status('[bold yellow]Thinking[/bold yellow]', spinner='balloon'):
             content = discuss.ask(query)
-        print(content)
+        console.print(Panel(content, title='🤖', style='prompt'))
